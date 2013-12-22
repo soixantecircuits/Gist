1376dd464ae4c2cea2f879c8c7e0dce118f7fdcc
gabrielstuff <gabriel@soixantecircuits.fr>
2013-12-22 19:38:48 +0100

    fixed #3 #4

--- WP_Appfog_easyInstall/wpinstall.sh
+++ WP_Appfog_easyInstall/wpinstall.sh
@@ -1,43 +1,69 @@
 #!/bin/sh
 
+prg=$0
+if [ ! -e "$prg" ]; then
+  case $prg in
+    (*/*) exit 1;;
+    (*) prg=$(command -v -- "$prg") || exit;;
+  esac
+fi
+dir=$(
+  cd -P -- "$(dirname -- "$prg")" && pwd -P
+) || exit
+prg=$dir/$(basename -- "$prg") || exit 
+
+realbin_path=`readlink $prg`
+dir_real=`dirname $realbin_path`
+
 if ! type "wp" > /dev/null 2>&1; then
   echo "wp-cli is required!"
   echo "Use 'curl https://raw.github.com/wp-cli/wp-cli.github.com/master/installer.sh | bash' to install it."
   exit
 fi
 
-if [ -d "$1" ] ; then
-  echo "Directory exist do you want to replace it ? yes/no"
+dir_resolve()
+{
+cd "$1" 2>/dev/null || return $?  # cd to desired directory; if fail, quell any error messages but return exit status
+echo "`pwd -P`" # output full, link-resolved path
+}
+
+if [ -d "`dir_resolve \"$1\"`" ] ; then
+  echo "Directory exist do you want to replace it or update? replace/update/cancel [replace]"
   read shouldReplace
-  		if [ "$shouldReplace" = "" ]; then
-			shouldReplace="yes"
-		fi
-		if [ "$shouldReplace" = "yes" ]; then
-			rm -rf $1
-			mkdir $1
-		else
-			exit 0
-		fi
+    if [ "$shouldReplace" = "" ] || [ "$shouldReplace" = "replace" ]; then
+      shouldReplace="yes"
+      rm -rf $1
+      mkdir $1
+    elif [ "$shouldReplace" = "update" ]; then
+     install_path="`dir_resolve \"$1\"`"
+     cd $1
+    else
+      exit 0
+    fi
 else 
-	if [ -z "$1" ];
-		then
-		echo "Try 'sh wpinstall.sh <Path/to/your/wordpress/folder>'"
-		exit
-	else
-		mkdir $1
-	fi
+  if [ -z "$1" ]; then
+    echo "Try 'sh wpinstall.sh <Path/to/your/wordpress/folder>'"
+    exit
+  else
+    mkdir $1
+  fi
 fi 
 
-install_path=$1
-
-echo "================================"
-echo "    Downloading Wordpress"
-echo "================================"
-
-#wp core download
-git clone git@repo.soixantecircuits.fr:wordpress/wordpressappfog.git $install_path
-
-cd $1
+if [ "$shouldReplace" = "yes" ]; then
+  install_path="`dir_resolve \"$1\"`"
+  echo "================================"
+  echo "    Downloading Wordpress"
+  echo "================================"
+  echo "Installing to $install_path"
+  #wp core download
+  git clone git@repo.soixantecircuits.fr:wordpress/wordpressappfog.git "$install_path"
+  cd $1
+else
+  echo "================================"
+  echo "    Updating Wordpress intall"
+  echo "================================"
+  echo "update/resume from $install_path"  
+fi
 
 echo "================================"
 echo "    Wordpress Configuration"
@@ -60,15 +86,29 @@ read dbprefix
 
 
 if [ "$dbhost" = "" ]; then
-	dbhost="localhost"
+  dbhost="localhost"
 fi
 if [ "$dbprefix" = "" ]; then
-	dbprefix="wp_"
+  dbprefix="wp_"
 fi
 
-wp core config --dbname=$dbname --dbuser="$dbuser" --dbpass="$dbpass" --dbhost="$dbhost" --dbprefix="$dbprefix" > /dev/null 2>&1
+wp core config --dbname=$dbname --dbuser="$dbuser" --dbpass="$dbpass" --dbhost="$dbhost" --dbprefix="$dbprefix" > 'log.txt' 2>&1
 
-wp db create "$dbname"
+echo "================================"
+echo "    Creating Database"
+echo "================================"
+
+RES=`wp db create > /dev/null 2>&1`
+echo "$RES" > log.txt
+
+if [[ $RES == *"Success"* ]]; then
+  echo "... Ok"
+elif [[ $RES == *"1007"* ]]; then
+  echo "... Database already exist... continuing"
+else
+  echo "An error occured please take a look at the log output."
+  exit
+fi
 
 echo "================================"
 echo "    Wordpress Installation"
@@ -110,9 +150,8 @@ wp plugin activate wpmandrill
 
 wp plugin activate wp-migrate-db-pro
 
-cd -
+cd - > /dev/null 2>&1
 php=`which php`
 wp_config_file="$install_path/wp-config.php"
-echo $wp_config_file
-php changeConfig.php $wp_config_file
-
+#echo "$dir_real/changeConfig.php"
+php "$dir_real/changeConfig.php" "$wp_config_file"
